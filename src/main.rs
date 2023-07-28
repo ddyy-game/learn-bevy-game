718d59ae7660bc5cc1ccdd506d3bb857f19b27ca
Yin Yu <yinyu3@xiaohongshu.com>
2023-07-28 16:43:36 +0800

    feat(app): load a scene at startup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use bevy::prelude::*;
 
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("models/Fox.glb#Scene0"),
+        ..default()
+    });
+    commands.spawn(Camera3dBundle::default());
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
